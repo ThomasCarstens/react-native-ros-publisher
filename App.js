258b118806443f2f02bb07d9329851bbc5b9d35a
txarst <thomaxarstens@gmail.com>
2022-05-24 14:30:06 +0200

    Simple connect

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { ROS } from './ROS' 
+import ToggleConnect from './ToggleConnect'
 export default function App() {
   return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ROS>
+      <ToggleConnect />
+    </ROS>
     </View>
   );
 }
---
+++ ToggleConnect.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { useROS } from './ROS'
+import { StyleSheet, Text, View, TextInput, Button, Flatlist} from 'react-native';
+
+function ToggleConnect() {
+  const { isConnected, topics, url, changeUrl, toggleConnection, toggleAutoconnect} = useROS();
+
+  // Try replacing this with your own unique rosbridge webserver address
+//   const defaultURL = "ws://192.168.1.26:11311";
+  const defaultURL = "ws://192.168.1.26:9090";
+	
+  // only runs once when ToggleConnect is first rendered (mounted)
+  useEffect(() => {
+    console.log('ToggleConnect is mounted!');
+    if (url !== defaultURL) {
+      changeUrl(defaultURL);
+    }
+
+    if (!isConnected) {
+      toggleAutoconnect();
+    }
+  },[])
+    
+  // runs every time there is an update to any state/rerender
+  useEffect(() => {
+    //console.log('rerender ToggleConnect');
+  })
+//   <TextInput
+//   style={styles.input}
+//   onChangeText={onChangeNumber}
+//   value={number}
+//   placeholder="useless placeholder"
+//   keyboardType="numeric"
+// />
+    console.log('result', topics)
+    const TOPICS = topics.map((topic, i) => { {topic.path} })
+    const [people, setPeople]=useState([
+        {name: 'shaun', key: '1'},
+        {name: 'yoshi', key: '2'},
+        {name: 'friend', key: '3'},
+
+    ])
+  return (
+
+        <View>
+        <Text>
+            Simple connect:  <Button onClick={ toggleConnection } title="Toggle Connect"/>  {"\n"}
+            ROS url input:  <TextInput name="urlInput" placeholder={ url } onChangeText={event => changeUrl(event.target.value)} />  {"\n"}
+            ROS url to connect to:  {url}  {"\n"}
+            Status of ROS: { isConnected ? "connected" : "not connected" }   {"\n"}
+            Topics detected:{"\n"}
+        </Text>
+        { topics.map((item) => {
+            return(
+                <View>
+                    <Text>{item.path} {item.type}</Text>
+                </View>
+            )
+            
+        })}
+
+
+         
+
+        {/* for (topic, i) in topics {
+        <Flatlist data = {[{key: i}
+                        ]}
+            renderItem={({topic, i}) => <Text> {i} :  {topic.path}</Text> }
+        />                       
+        } */}
+ 
+        {/* } */}
+
+        
+        </View>
+
+
+  );
+}
+
+export default ToggleConnect;
